1c3c3bdce0ef5f341d6b9d335ce8cec2c401b293
greyburn <greyburn@gmail.com>
2025-08-08 16:46:53 -0600

    Update __init__.py

--- esphome/components/lgap/climate/__init__.py
+++ esphome/components/lgap/climate/__init__.py
@@ -30,7 +30,6 @@ CONFIG_SCHEMA = climate.climate_schema(
 ).extend(cv.COMPONENT_SCHEMA)
 
 
-
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
